ab641a443ce18ea2aed47d682947fefb88e44eea
rakeshsangadikar <rgsangadikar168@gmail.com>
2021-12-25 23:44:11 +0530

    convert to maven

---
+++ src/main/java/com/management/mcams/backend/MCAMSApplication.java
@@ -0,0 +1,13 @@
+package com.management.mcams.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCAMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MCAMSApplication.class, args);
+	}
+
+}
